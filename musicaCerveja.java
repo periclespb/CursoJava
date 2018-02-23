238b7260f0662d0e369e2ef619fa57dacfbcc930
periclespb <36752459+periclespb@users.noreply.github.com>
2018-02-23 14:15:16 -0300

    Add files via upload

---
+++ musicaCerveja.java
@@ -0,0 +1,31 @@
+
+public class musicaCerveja {
+	
+	public static void main(String[]args) {
+		
+		int x=99;
+		int y;
+		int z;
+		
+		
+		if (x==99) {
+			System.out.println("cervejas no freezer "+x);
+		}
+		
+		y=x-1;
+		
+		if(y==98) {
+			System.out.println("pego uma garrafa,passo para frente");
+			System.out.println("cervejas no freezer "+y);
+		}
+		
+		z=y-1;
+		
+		if(z==97) {
+			System.out.println("pego uma garrafa,passo para frente");
+			System.out.println(" garrafas no frezzer "+z);
+		}
+		
+	}
+
+}
